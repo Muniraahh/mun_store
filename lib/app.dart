41eac7bebea789866eeb6dbb9e86ed3c22b6f623
Munira M Patel <ptlmun2088@gmail.com>
2025-02-11 11:13:24 +0530

    onboarding done

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mun_store/splash_screen.dart';
 import 'package:mun_store/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
@@ -6,10 +8,12 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: MAppTheme.lightTheme,
       darkTheme: MAppTheme.darkTheme,
+      home: const SplashScreen(), // Starts with SplashScreen
     );
   }
 }
