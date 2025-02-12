3c09223f89bcb3f4f0e2b91e6b3c599a6adfe9c0
irfana <patelirfana567@gmail.com>
2025-02-12 12:37:38 +0530

    verifyemailid

--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:mun_store/features/authentication/screens/signup/verify_email.dart';
 import 'package:mun_store/features/authentication/screens/signup/widgets/terms_conditions_checkbox.dart';
 import 'package:mun_store/utils/constants/sizes.dart';
 import 'package:mun_store/utils/constants/text_strings.dart';
@@ -67,7 +69,11 @@ class SignupForm extends StatelessWidget {
             const SizedBox(height: MSizes.spaceBetweenSections),
 
             ///signup button
-            SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () {}, child: const Text(MTexts.createAccount))),
+            SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () => Get.to(() => const VerifyEmailScreen() ),
+                    child: const Text(MTexts.createAccount))),
           ],
         )
     );
